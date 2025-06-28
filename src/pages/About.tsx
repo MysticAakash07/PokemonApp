c6eea1ab08a0a36ced7221c9f8dec718b970a948
MysticAakash07 <rahulsagar460@gmail.com>
2025-06-28 14:47:22 +0530

    feat: complete About page layout and content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,28 @@
 import Wrapper from "../sections/Wrapper"
-
+import avatarImage from "../assets/Aakash.png"
+import { FaGithub, FaLinkedin, FaCode } from "react-icons/fa"
 function About() {
-  return <div>About</div>
+  return (
+    <div className="profile">
+      <img src={avatarImage} alt="profile image" className="profile-image"/>
+      <h1 className="profile-text">Hi I am Aakash</h1>
+      <h2 className="profile-text">The creator of this cool Pokedex</h2>
+      <h4 className="profile-text">
+        This project is created for Learning Purposes
+      </h4>
+      <div className="profile-links">
+        <a href="https://www.github.com/MysticAakash07">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/MysticAakash">
+          <FaLinkedin />
+        </a>
+        <a href="https://www.leetcode.com/Mystic_Aakash">
+          <FaCode />
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Wrapper(About)
